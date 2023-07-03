2d54cf7d31788cec5dbca0100f79ee17218cebfe
Salandora <salandora@gmail.com>
2023-07-03 18:27:52 +0200

    Remove unused mixins

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/ItemStackMixin.java
+++
@@ -1,9 +0,0 @@
-package net.p3pp3rf1y.sophisticatedcore.mixin;
-
-import net.minecraft.world.item.ItemStack;
-import net.p3pp3rf1y.sophisticatedcore.extensions.item.SophisticatedItemStack;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ItemStack.class)
-public class ItemStackMixin implements SophisticatedItemStack {
-}
