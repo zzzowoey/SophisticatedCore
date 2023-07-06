93e07315ac7d1cf6202ef0a95920700f5cf0feb1
Salandora <salandora@gmail.com>
2023-07-06 23:44:47 +0200

    Fix the XP fluid not showing up correctly in jei

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/client/init/ModFluids.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/client/init/ModFluids.java
@@ -11,8 +11,8 @@ import static net.p3pp3rf1y.sophisticatedcore.init.ModFluids.XP_STILL;
 public class ModFluids {
     public static void registerFluids() {
         FluidRenderHandlerRegistry.INSTANCE.register(XP_STILL.get(), XP_FLOWING.get(), new SimpleFluidRenderHandler(
-                new ResourceLocation(SophisticatedCore.ID, "fluids/xp_still"),
-                new ResourceLocation(SophisticatedCore.ID, "fluids/xp_flowing")
+                new ResourceLocation(SophisticatedCore.ID, "block/xp_still"),
+                new ResourceLocation(SophisticatedCore.ID, "block/xp_flowing")
         ));
     }
 }
--- src/main/java/net/p3pp3rf1y/sophisticatedcore/init/ModFluids.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/init/ModFluids.java
@@ -3,8 +3,14 @@ package net.p3pp3rf1y.sophisticatedcore.init;
 import io.github.fabricators_of_create.porting_lib.util.LazyRegistrar;
 import io.github.fabricators_of_create.porting_lib.util.RegistryObject;
 import io.github.fabricators_of_create.porting_lib.util.SimpleFlowableFluid;
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalFluidTags;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariantAttributeHandler;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariantAttributes;
+import net.minecraft.Util;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.material.FlowingFluid;
@@ -22,6 +28,7 @@ public class ModFluids {
 	public static final ResourceLocation EXPERIENCE_TAG_NAME = new ResourceLocation("forge:experience");
 
 	public static final TagKey<Fluid> EXPERIENCE_TAG = TagKey.create(Registries.FLUID, EXPERIENCE_TAG_NAME);
+
 	public static final LazyRegistrar<Fluid> FLUIDS = LazyRegistrar.create(BuiltInRegistries.FLUID, SophisticatedCore.ID);
 
 	public static final RegistryObject<FlowingFluid> XP_STILL = FLUIDS.register("xp_still", () -> new SimpleFlowableFluid.Still(fluidProperties()));
@@ -31,5 +38,12 @@ public class ModFluids {
 	
 	public static void registerHandlers() {
 		FLUIDS.register();
+
+		FluidVariantAttributes.register(XP_STILL.get(), new FluidVariantAttributeHandler() {
+			@Override
+			public Component getName(FluidVariant fluidVariant) {
+				return Component.translatable(Util.makeDescriptionId("fluid", BuiltInRegistries.FLUID.getKey(fluidVariant.getFluid())));
+			}
+		});
 	}
 }
