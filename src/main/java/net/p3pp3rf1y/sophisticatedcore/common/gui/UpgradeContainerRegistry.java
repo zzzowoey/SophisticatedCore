3e00a51c2ebe0328c9e64b1720f31190ef48c5b4
Salandora <salandora@gmail.com>
2023-11-07 19:01:13 +0100

    Remove deprecated code

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/UpgradeContainerRegistry.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/UpgradeContainerRegistry.java
@@ -17,7 +17,7 @@ public class UpgradeContainerRegistry {
 	private static final Map<ResourceLocation, UpgradeContainerType<? extends IUpgradeWrapper, ? extends UpgradeContainerBase<?, ?>>> UPGRADE_CONTAINERS = new HashMap<>();
 
 	public static void register(Item item, UpgradeContainerType<? extends IUpgradeWrapper, ? extends UpgradeContainerBase<?, ?>> containerFactory) {
-		register(item.builtInRegistryHolder().key().location(), containerFactory);
+		register(BuiltInRegistries.ITEM.getKey(item), containerFactory);
 	}
 	public static void register(ResourceLocation upgradeName, UpgradeContainerType<? extends IUpgradeWrapper, ? extends UpgradeContainerBase<?, ?>> containerFactory) {
 		UPGRADE_CONTAINERS.put(upgradeName, containerFactory);
