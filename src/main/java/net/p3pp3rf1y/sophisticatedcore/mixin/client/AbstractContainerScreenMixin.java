3fb4ad48703199071e923c46211c345e7c0e1e54
Salandora <salandora@gmail.com>
2023-12-05 23:33:31 +0100

    [FIX]: Inventory scroll panel was unable to set the hovered slot causing tooltip and open backpack hotkey to not function

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/client/AbstractContainerScreenMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/client/AbstractContainerScreenMixin.java
@@ -1,10 +1,14 @@
 package net.p3pp3rf1y.sophisticatedcore.mixin.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.core.NonNullList;
@@ -14,6 +18,9 @@ import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageContainerMenuBase;
 import net.p3pp3rf1y.sophisticatedcore.extensions.client.gui.screens.inventory.SophisticatedAbstractContainerScreen;
 import net.p3pp3rf1y.sophisticatedcore.util.MixinHelper;
 
+import java.util.function.Supplier;
+import org.jetbrains.annotations.Nullable;
+
 @Mixin(AbstractContainerScreen.class)
 public abstract class AbstractContainerScreenMixin implements SophisticatedAbstractContainerScreen {
     @Shadow
@@ -22,7 +29,11 @@ public abstract class AbstractContainerScreenMixin implements SophisticatedAbstr
     @Shadow
     protected int topPos;
 
-    @Override
+	@Shadow
+	@Nullable
+	protected Slot hoveredSlot;
+
+	@Override
     public int getGuiLeft() {
         return leftPos;
     }
@@ -37,21 +48,39 @@ public abstract class AbstractContainerScreenMixin implements SophisticatedAbstr
         return MixinHelper.cast(this);
     }
 
+	@Unique
+	private <T> T ifStorageScreenBase(Supplier<T> value, Supplier<T> elseValue) {
+		return getSelf() instanceof StorageScreenBase ? value.get() : elseValue.get();
+	}
+	@Unique
+	private void ifStorageScreenBase(Runnable value, Runnable elseValue) {
+		if (getSelf() instanceof StorageScreenBase) {
+			value.run();
+		} else {
+			elseValue.run();
+		}
+	}
+
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;size()I"))
     private int sophisticatedcore$MenuSlotSize(NonNullList<Slot> instance) {
-        if (getSelf() instanceof StorageScreenBase) {
-            return StorageContainerMenuBase.NUMBER_OF_PLAYER_SLOTS;
-        }
-
-        return instance.size();
+		return ifStorageScreenBase(() -> StorageContainerMenuBase.NUMBER_OF_PLAYER_SLOTS, instance::size);
     }
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;get(I)Ljava/lang/Object;"))
     private Object sophisticatedcore$MenuSlotGet(NonNullList<Slot> instance, int i) {
-        if (getSelf() instanceof StorageScreenBase) {
-            return getSelf().getMenu().getSlot(((StorageScreenBase<? extends StorageContainerMenuBase<?>>)getSelf()).getMenu().getInventorySlotsSize() - StorageContainerMenuBase.NUMBER_OF_PLAYER_SLOTS + i);
-        }
-
-        return instance.get(i);
+			return ifStorageScreenBase(() -> {
+				StorageContainerMenuBase<?> menu = ((StorageScreenBase<? extends StorageContainerMenuBase<?>>)getSelf()).getMenu();
+				return menu.getSlot(menu.getInventorySlotsSize() - StorageContainerMenuBase.NUMBER_OF_PLAYER_SLOTS + i);
+			}, () -> instance.get(i));
     }
+
+	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V", shift = At.Shift.BEFORE))
+	private void sophisticatedcore$resetHoveredSlot(PoseStack poseStack, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+		ifStorageScreenBase(() -> this.hoveredSlot = null, () -> {});
+	}
+
+	@Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screens/inventory/AbstractContainerScreen;hoveredSlot:Lnet/minecraft/world/inventory/Slot;", opcode = Opcodes.PUTFIELD, ordinal = 0))
+	private void sophisticatedcore$patchHoveredSlot(AbstractContainerScreen<?> instance, Slot value) {
+		ifStorageScreenBase(() -> {}, () -> instance.hoveredSlot = value);
+	}
 }
